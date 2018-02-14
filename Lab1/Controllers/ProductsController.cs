0e58937a5d8dae1f246bb8f2b1d3560d1094a796
manjeet101297 <35846554+manjeet101297@users.noreply.github.com>
2018-02-13 21:29:03 -0500

    Lab1

---
+++ Lab1/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(string product)
+        {
+            ViewBag.Product = product;
+            ViewBag.ProductDetails = "You selected product: "+product;
+            return View();
+        }
+    }
+}
